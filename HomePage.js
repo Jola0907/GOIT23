6e4f701daa737bcd95b7143f4c4f64f8f4aea1fc
Jolanta Krawczyk <115967036+Jola0907@users.noreply.github.com>
2023-04-15 17:59:49 +0200

    Add files via upload

---
+++ HomePage.js
@@ -0,0 +1,11 @@
+import { homeStr } from "../fixtures/pages/HomePage";
+
+const  homStr = new homeStr
+
+describe("strHom", () => {
+         it("homStrona", () => {
+          
+            homStr.navigate();
+
+        });
+    });
---
+++ login.js
@@ -0,0 +1,20 @@
+import { logowanie2user } from "../../e2e/log.cy" 
+
+const logUse = new logowanie2user
+        
+    describe("LogInOut", () => {
+        it("Log2User", () => {
+         
+            logowanie2user.logowanieUserFirst();
+
+            logowanie2user.logowanieUserSecond();
+
+            logowanie2user.menuStrony();
+
+            logowanie2user.logOut();
+
+
+       });
+   });
+    
+
