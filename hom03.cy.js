291ccaadc52a7b1c94bb2a9e70d3af6938a80a07
Jolanta Krawczyk <115967036+Jola0907@users.noreply.github.com>
2023-04-17 21:20:18 +0200

    Update hom03.cy.js

--- hom03.cy.js
+++ hom03.cy.js
@@ -2,5 +2,8 @@ export class homeStr {
     navigate() {
         cy.visit("https://www.edu.goit.global/account/login")
     }
+     logOut() {
+        
+        cy.get('[type="submit"]').should('have.text', 'Log out').click()
     
-}
+}}
